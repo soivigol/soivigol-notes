b23e489013cec28ef3595c0200ce6413c35ac3a1
soivigol <david@davidvina.es>
2023-05-21 11:10:25 +0000

    Fix space in Button WorkFlow

--- build/flow/index.asset.php
+++ build/flow/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '2a77257654bc1aeb7a86');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'f505d74e100b3bc38210');

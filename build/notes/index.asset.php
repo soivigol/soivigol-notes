25d11a36e73ab0a769fe6b180d751ade2f4cdc92
soivigol <david@davidvina.es>
2023-05-21 10:49:26 +0000

    Compile to production

--- build/notes/index.asset.php
+++ build/notes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '05a3b51b4440b7f04221');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '958671cac1cec6ce5da6');

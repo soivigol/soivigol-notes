e7b7077d87ab7a99e2e8c82211963691e2f015c4
soivigol <david@davidvina.es>
2023-05-22 15:57:38 +0000

    Credits

--- soivigol-notes.php
+++ soivigol-notes.php
@@ -5,7 +5,9 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           1.0.1
+ * Plugin Uri:        https://sovigol.dev/plugin-soivigol-notes
  * Author:            David Viña
+ * Author URI:        https://soivigol.dev
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       soivigol-notes
